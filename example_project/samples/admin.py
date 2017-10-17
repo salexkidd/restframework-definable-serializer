bcebeaecdb620b59a9bee13d492dd6fbba1f2c5c
salexkidd <salexkidd@gmail.com>
2017-10-17 22:20:36 +0900

    Remove example project

--- example_project/samples/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from definable_serializer.admin import DefinableSerializerAdmin
-
-from . import models as samples_models
-
-
-@admin.register(samples_models.Sample)
-class Sample(DefinableSerializerAdmin):
-    ...
